94ff7962925815b0c150b484097398ed977286ae
Sunny <89862130+sunworms@users.noreply.github.com>
2025-09-21 19:03:02 +0530

    Update boot.nix

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -3,6 +3,7 @@
 {
   boot = {
     loader.systemd-boot.enable = true;
+    loader.systemd-boot.configurationLimit = 5;
     loader.efi.canTouchEfiVariables = true;
     kernelPackages = pkgs.linuxPackages_latest;
   };
