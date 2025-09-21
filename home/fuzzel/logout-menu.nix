eb7e650776313e65b8206be99e057ff6ec82fc09
Sunny <89862130+sunworms@users.noreply.github.com>
2025-09-21 21:23:07 +0530

    Update logout-menu.nix

--- home/fuzzel/logout-menu.nix
+++ home/fuzzel/logout-menu.nix
@@ -4,16 +4,15 @@
   home.packages = with pkgs; [
     (writeShellScriptBin "logout-menu" ''
     # Define menu options
-    options=" Lock\n󰍃 Logout\n⏾ Suspend\n Hibernate\n Reboot\n Shutdown"
+    options=" Lock\n󰍃 Logout\n⏾ Suspend\n Reboot\n Shutdown"
 
     # Show menu with fuzzel
-    choice=$(echo -e "$options" | fuzzel --dmenu --prompt="System: " --lines=6)
+    choice=$(echo -e "$options" | fuzzel --dmenu --prompt="System: " --lines=5)
 
     case "$choice" in
         " Lock") swaylock ;; # or your locker, e.g. swaylock, hyprlock
         "󰍃 Logout") niri msg action quit --skip-confirmation ;; # or hyprctl dispatch exit
         "⏾ Suspend") systemctl suspend ;;
-        " Hibernate") systemctl hibernate ;;
         " Reboot") systemctl reboot ;;
         " Shutdown") systemctl poweroff ;;
         *) exit 0 ;;
