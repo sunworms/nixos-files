8b1b78d3123d5ec07a82bdfb93cce52f5a2f5415
Sunny <sunnybhowmick0310@gmail.com>
2025-09-23 23:06:09 +0530

    go back to virtualbox :(

--- system/packages/virtualbox.nix
+++ system/packages/virtualbox.nix
@@ -1,12 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    virtualbox
+  ];
+
+  boot.kernelModules = [
+    "vboxdrv"
+    "vboxnetflt"
+    "vboxnetadp"
+  ];
   virtualisation.virtualbox.host = {
     enable = true;
     enableKvm = true;
     addNetworkInterface = false;
     enableHardening = false;
-    enableExtensionPack = true;
   };
   users.extraGroups.vboxusers.members = [ "sunny" ];
 }
