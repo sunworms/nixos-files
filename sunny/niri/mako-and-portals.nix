4f8a7e955a35ac94419f66111cdbdb413b36bbf2
Sunny <sunnybhowmick0310@gmail.com>
2025-09-28 15:46:32 +0530

    add swaylock config

--- sunny/niri/mako-and-portals.nix
+++ sunny/niri/mako-and-portals.nix
@@ -21,5 +21,42 @@
     org.freedesktop.impl.portal.FileChooser=gtk
     org.freedesktop.impl.portal.Secret=gnome-keyring
     '';
+
+    ".config/swaylock/config".text = ''
+    daemonize
+    ignore-empty-password
+    image=$HOME/Pictures/walls/nix-blur.png
+    font=D2CodingLigature Nerd Font
+    font-size=14
+    color=161616
+    bs-hl-color=42be65
+    caps-lock-bs-hl-color=ffc726
+    caps-lock-key-hl-color=ffc726
+    inside-color=00000000
+    inside-clear-color=00000000
+    inside-caps-lock-color=00000000
+    inside-ver-color=00000000
+    inside-wrong-color=00000000
+    key-hl-color=42be65
+    layout-bg-color=00000000
+    layout-border-color=00000000
+    layout-text-color=f2f4f8
+    line-color=00000000
+    line-clear-color=00000000
+    line-caps-lock-color=00000000
+    line-ver-color=00000000
+    line-wrong-color=00000000
+    ring-color=393939
+    ring-clear-color=42be65
+    ring-caps-lock-color=ffc726
+    ring-ver-color=33b1ff
+    ring-wrong-color=ff7eb6
+    separator-color=00000000
+    text-color=f2f4f8
+    text-clear-color=42be65
+    text-caps-lock-color=ffc726
+    text-ver-color=ffc726
+    text-wrong-color=ff7eb6
+    '';
   };
 }
