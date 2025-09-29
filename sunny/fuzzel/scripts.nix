12c356423a61b5b2494ac0ede074b8eb71126082
Sunny <sunnybhowmick0310@gmail.com>
2025-09-29 13:20:16 +0530

    edit fuzzel

--- sunny/fuzzel/scripts.nix
+++ sunny/fuzzel/scripts.nix
@@ -4,17 +4,17 @@
   packages = with pkgs; [
     (writeShellScriptBin "logout-menu" ''
     # Define menu options
-    options=" Lock\n󰍃 Logout\n⏾ Suspend\n Reboot\n Shutdown"
+    options="  Lock\n󰍃  Logout\n⏾  Suspend\n  Reboot\n  Shutdown"
 
     # Show menu with fuzzel
     choice=$(echo -e "$options" | fuzzel --dmenu --prompt="System: " --lines=5)
 
     case "$choice" in
-        " Lock") swaylock ;; # or your locker, e.g. swaylock, hyprlock
-        "󰍃 Logout") niri msg action quit --skip-confirmation ;; # or hyprctl dispatch exit
-        "⏾ Suspend") systemctl suspend ;;
-        " Reboot") systemctl reboot ;;
-        " Shutdown") systemctl poweroff ;;
+        "  Lock") swaylock ;; # or your locker, e.g. swaylock, hyprlock
+        "󰍃  Logout") niri msg action quit --skip-confirmation ;; # or hyprctl dispatch exit
+        "⏾  Suspend") systemctl suspend ;;
+        "  Reboot") systemctl reboot ;;
+        "  Shutdown") systemctl poweroff ;;
         *) exit 0 ;;
     esac
     '')
