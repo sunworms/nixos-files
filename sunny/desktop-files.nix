4624d642ed319b10540a7842eb9ccf4b7c7845a5
Sunny <sunnybhowmick0310@gmail.com>
2025-09-29 20:07:56 +0530

    add pcsx2 entry

--- sunny/desktop-files.nix
+++ sunny/desktop-files.nix
@@ -1,7 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
-  file.home.".local/share/applications/spotify.desktop".text = ''
+  file.home = {
+    ".local/share/applications/spotify.desktop".text = ''
     [Desktop Entry]
     Categories=Audio;Music;Player;AudioVideo
     Exec=spotify %U --ozone-platform-hint=auto
@@ -12,4 +13,20 @@
     Terminal=false
     Type=Application
     '';
+
+    ".local/share/applications/pcsx2.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=PCSX2
+    GenericName="PlayStation 2 emulator"
+    Comment="PlayStation 2 emulator"
+    TryExec=pcsx2
+    Exec=pcsx2
+    Icon=pcsx2
+    Terminal=false
+    StartupNotify=true
+    StartupWMClass=pcsx2
+    Categories=Game;Emulator;
+    '';
+  };
 }
