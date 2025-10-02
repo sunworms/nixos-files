7bc9f1b99a6dfec0ba4685524c4adcda81a34e59
Sunny <sunnybhowmick0310@gmail.com>
2025-10-02 18:46:12 +0530

    add secrets via git-crypt

--- system/core/users.nix
+++ system/core/users.nix
@@ -5,7 +5,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     shell = pkgs.zsh;
-    hashedPassword = "$6$L07PMcHqVkf0Bvrt$IrMp1dr7ZvVc1Kf6TFqTpuQgyfr1HypoBSDt3MkiBWkiiCxwXTs49du5mMYKlp6CL.pMACi7K9gb7yAowbHF70";
+    hashedPassword = "$6$G9FZgSeZBXAlxmsZ$RmdDwZ.snMkYY5y6e/gLFG.ga4bw8B.bowaHQ6.CPyiqXj1kVDE/sgcCODThToSKsSTW6IQLl3Odtcmp36p.h/";
   };
 
   programs.zsh.enable = true;
