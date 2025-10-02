02757c45cfc62103b4bdff47ac58651243911c4b
Sunny <sunnybhowmick0310@gmail.com>
2025-10-02 19:45:26 +0530

    remove hashed password

--- system/core/users.nix
+++ system/core/users.nix
@@ -5,7 +5,6 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     shell = pkgs.zsh;
-    hashedPassword = "$6$G9FZgSeZBXAlxmsZ$RmdDwZ.snMkYY5y6e/gLFG.ga4bw8B.bowaHQ6.CPyiqXj1kVDE/sgcCODThToSKsSTW6IQLl3Odtcmp36p.h/";
   };
 
   programs.zsh.enable = true;
