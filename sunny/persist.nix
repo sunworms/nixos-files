0af4c82140a3f305d2177eb8bb4e55434eda1fd4
Sunny <89862130+sunworms@users.noreply.github.com>
2025-10-04 00:50:16 +0530

    Create persist.nix

---
+++ sunny/persist.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+{
+  home.persistence."/persist/home/sunny" = {
+    directories = [
+      "nixos-files"
+      "Documents"
+      "Games"
+      "Music"
+      "Pictures"
+      "Videos"
+      "VMs"
+      ".mozilla"
+    ];
+    files = [
+      ".p10k.zsh"
+      ".zsh_history"
+      ".config/katerc"
+      ".config/discord/Cookies"
+      ".config/jamesdsp/audio.conf"
+      ".config/jamesdsp/application.conf"
+      ".config/spotify/prefs"
+    ];
+  };
+}
