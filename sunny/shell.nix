5276d8726deed4fba189bf5b3a5e3e0d54a26cc7
Sunny <sunnybhowmick0310@gmail.com>
2025-10-04 18:35:43 +0530

    remove p10k

--- sunny/shell.nix
+++ sunny/shell.nix
@@ -5,12 +5,6 @@
     enable = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    plugins = [
-    {
-      name = "powerlevel10k";
-      src = pkgs.zsh-powerlevel10k;
-      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    }];
     shellAliases = {
       grep = "grep --color=auto";
       warpon ="warp-cli connect";
