3306b6270f836d26ccfcefbd146c8d6e0f8b27bb
Sunny <sunnybhowmick0310@gmail.com>
2025-10-04 16:44:18 +0530

    update passwords

--- system/core/users.nix
+++ system/core/users.nix
@@ -1,10 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
+  users.users.root.hashedPassword = "$6$St8WQDhQfAAapkrb$gXXk/whalGPU7MMH5IlvHdt/pU88In3o8V8e3ftEy9o55zYYcjJnHHR8krxaOUFPOY8WQR2sLyf4h1JHtGIGv/";
+
   users.users.sunny = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     shell = pkgs.zsh;
+    hashedPassword = "$6$sOBnL9kL3l2t2OHa$mc8ujER7TQUTbRyv0PVWF8WWQMNY.4UCEBBriE1KRuQfImesk2Ulg5kVRPZY9eKYkNzJ6urcN3oYTkTrh116u1";
   };
 
   programs.zsh.enable = true;
