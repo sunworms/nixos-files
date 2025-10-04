ac8b9de40462e06b21129800a38f62eb273eebe3
Sunny <sunnybhowmick0310@gmail.com>
2025-10-04 22:40:33 +0530

    fix permissions

--- system/core/persist.nix
+++ system/core/persist.nix
@@ -42,10 +42,27 @@
     };
   };
 
-  systemd.tmpfiles.rules = [
-    "d /home/sunny      0755  sunny users - -"
-    "d /home/sunny/.config 0755 sunny users - -"
-    "d /home/sunny/.local 0755 sunny users - -"
-    "d /home/sunny/.local/share 0755 sunny users - -"
-  ];
+  systemd.services.fix-config-perms = {
+    description = "Fix recursive permissions for ~/.config";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        /run/current-system/sw/bin/chown -R sunny:users /home/sunny/.config
+        /run/current-system/sw/bin/chmod -R 0755 /home/sunny/.config
+      '';
+    };
+  };
+
+  systemd.services.fix-local-perms = {
+    description = "Fix recursive permissions for ~/.local";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        /run/current-system/sw/bin/chown -R sunny:users /home/sunny/.local
+        /run/current-system/sw/bin/chmod -R 0755 /home/sunny/.local
+      '';
+    };
+  };
 }
