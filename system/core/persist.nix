a6f5aa2bb057062f663629e527bab86afc0de190
Sunny <89862130+sunworms@users.noreply.github.com>
2025-10-04 17:47:13 +0530

    Update persist.nix

--- system/core/persist.nix
+++ system/core/persist.nix
@@ -9,7 +9,6 @@
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd"
-      { directory = "/home/sunny"; user = "sunny"; group = "users"; mode = "0755"; }
     ];
     files = [
       "/etc/machine-id"
