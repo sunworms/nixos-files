b4ab0dfc57b05d354ac5cae41049573f6ea01880
Sunny <sunnybhowmick0310@gmail.com>
2025-10-04 21:12:55 +0530

    switch from nix to lix

--- system/core/nix-settings.nix
+++ system/core/nix-settings.nix
@@ -1,7 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
+  nixpkgs.overlays = [ (final: prev: {
+    inherit (prev.lixPackageSets.stable)
+      nixpkgs-review
+      nix-eval-jobs
+      nix-fast-build
+      colmena;
+  }) ];
+
   nix = {
+    package = pkgs.lixPackageSets.stable.lix;
     channel.enable = false;
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
