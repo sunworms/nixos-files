c5b490c970268f0e9ba9d76071510a07078395d4
Sunny <sunnybhowmick0310@gmail.com>
2025-10-05 18:03:06 +0530

    make regreet work with niri

--- system/packages/desktop.nix
+++ system/packages/desktop.nix
@@ -1,9 +1,57 @@
 { config, lib, pkgs, ... }:
 
+let
+  niriConfig = pkgs.writeText "greetd-niri-config" ''
+    hotkey-overlay { skip-at-startup; }
+    input {
+      keyboard {
+        repeat-delay 600
+        repeat-rate 25
+        track-layout "global"
+        numlock
+      }
+      touchpad {
+        tap
+        drag true
+        natural-scroll
+        scroll-method "two-finger"
+        click-method "clickfinger"
+      }
+      mod-key "Super"
+    }
+    spawn-sh-at-startup "${pkgs.regreet}/bin/regreet; niri msg action quit --skip-confirmation"
+    binds {
+      Mod+Shift+S { spawn "systemctl" "poweroff"; }
+      Mod+Shift+R { spawn "systemctl" "reboot"; }
+    }
+    cursor {
+        xcursor-theme "Vimix-cursors"
+        xcursor-size 24
+    }
+  '';
+in
 {
   services = {
     libinput.enable = true;
-    displayManager.ly.enable = true;
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.niri-unstable}/bin/niri --config ${niriConfig}";
+        };
+      };
+    };
+  };
+
+  programs.regreet = {
+    enable = true;
+    iconTheme = {
+      name = "Tela circle black dark";
+      package = (pkgs.tela-circle-icon-theme.override {
+        colorVariants = [ "black" ];
+      });
+    };
+    theme.name = "adw-gtk3";
   };
 
   programs.niri = {
