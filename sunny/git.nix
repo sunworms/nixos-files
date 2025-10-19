aecb494b691a80af8b97cf4cfef96cc0537f6e77
Sunny <sunnybhowmick0310@gmail.com>
2025-10-19 16:35:51 +0530

    remove github token, change connections to ssh

--- sunny/git.nix
+++ sunny/git.nix
@@ -1,19 +1,9 @@
 { config, ... }:
 
-let
-  githubToken = builtins.readFile config.sops.secrets."sunny/github/token".path;
-in
 {
   programs.git = {
     enable = true;
     userEmail = "sunnybhowmick0310@gmail.com";
     userName = "Sunny";
-    extraConfig = {
-      url = {
-        "https://oauth2:${githubToken}@github.com" = {
-          insteadOf = "https://github.com";
-        };
-      };
-    };
   };
 }
