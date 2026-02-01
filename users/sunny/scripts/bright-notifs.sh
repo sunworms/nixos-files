8a1eb4b13028fe9a0ebb9eb02b7722a90942290d
Sunny <sunnybhowmick0310@gmail.com>
2026-02-01 21:12:58 +0530

    deleted unneeded scripts folder

--- users/sunny/scripts/bright-notifs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-brightness=$(brightnessctl get)
-max=$(brightnessctl max)
-percent=$((brightness * 100 / max))
-
-notify-send -a "Brightness" -h int:value:$percent -h string:x-canonical-private-synchronous:brightness "Brightness: ${percent}%"
