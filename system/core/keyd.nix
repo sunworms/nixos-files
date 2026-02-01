5748eb41486a882393cb5cf15a04a0a034bde35c
Sunny <sunnybhowmick0310@gmail.com>
2026-02-01 21:06:36 +0530

    removed some bindings to make gnome bindings work

--- system/core/keyd.nix
+++ system/core/keyd.nix
@@ -8,11 +8,6 @@
         ids = [ "*" ];
         settings = {
           main = {
-            shift = "oneshot(shift)";
-            meta = "oneshot(meta)";
-            control = "oneshot(control)";
-            leftalt = "oneshot(alt)";
-            rightalt = "oneshot(altgr)";
             capslock = "overload(control, esc)";
             esc = "capslock";
           };
