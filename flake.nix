1a4bdc35ba49e4759a557cb71f67f8189cd94228
Sunny <sunnybhowmick0310@gmail.com>
2026-02-02 21:40:47 +0530

    remove zen browser

--- flake.nix
+++ flake.nix
@@ -13,11 +13,6 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser-flake = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "";
-    };
     quickshell = {
       url = "github:quickshell-mirror/quickshell?ref=1e4d804e7f3fa7465811030e8da2bf10d544426a";
       inputs.nixpkgs.follows = "nixpkgs";
