dce7bd02cf5e39164571483a9f6946f0269cea83
Sunny <sunnybhowmick0310@gmail.com>
2026-02-02 16:06:14 +0530

    moved environment variables to niri config

--- users/sunny/default.nix
+++ users/sunny/default.nix
@@ -9,22 +9,6 @@
 
   directory = "/home/sunny";
 
-  environment.sessionVariables = {
-    CLUTTER_BACKEND = "wayland,x11,*";
-    GDK_BACKEND = "wayland,x11,*";
-    NIXOS_OZONE_WL = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_QPA_PLATFORM = "wayland";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    SDL_VIDEODRIVER = "wayland,x11,*";
-    _JAVA_AWT_WM_NONREPARENTING = "1";
-    EDITOR = "emacsclient";
-    SUDO_EDITOR = "emacsclient";
-    VISUAL = "emacsclient";
-    GTK_THEME = "adw-gtk3";
-    QT_QPA_PLATFORMTHEME = "gtk3";
-  };
-
   packages = with pkgs; [
     fishPlugins.tide
     fishPlugins.git-abbr
