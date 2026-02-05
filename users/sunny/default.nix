2340c76d646801e5e4e8367e3a8ca718b1681300
Sunny <sunnybhowmick0310@gmail.com>
2026-02-05 18:27:41 +0530

    add a autostart desktop file for budgie settings

--- users/sunny/default.nix
+++ users/sunny/default.nix
@@ -28,7 +28,21 @@
       ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
     '')
 
+    # budgie settings
+    (writeShellScriptBin "apply-gtk-settings-budgie" ''
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme "'Qogir-Dark'"
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/icon-theme "'Qogir-Dark'"
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/cursor-theme "'Qogir'"
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/cursor-size 24
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme/monospace-font-name "'D2CodingLigature Nerd Font Mono 11'"
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/background/picture-uri "'file://${pkgs.budgie-backgrounds}/share/backgrounds/budgie/singaporean-cityscape.jpg'"
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/screensaver/picture-uri "'file://${pkgs.budgie-backgrounds}/share/backgrounds/budgie/singaporean-cityscape.jpg'"
+    '')
+
     #image viewer
     swayimg
   ];
+
+  xdg.config.files."autostart/apply-gtk-settings-budgie.desktop".source = ./apply-gtk-settings-budgie.desktop;
 }
