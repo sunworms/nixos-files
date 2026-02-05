0d34499f4d7c619bc329b1b6f08de83e248b27e2
Sunny <sunnybhowmick0310@gmail.com>
2026-02-05 15:08:03 +0530

    fetch only non-github repos with fetchit

--- default.nix
+++ default.nix
@@ -3,17 +3,22 @@ let
 
   sources = builtins.mapAttrs (name: value:
     let
-      src = if value.src.type == "git" then
-        fetchGit {
-          url = "${value.src.url}";
-          rev = "${value.src.rev}";
-          shallow = true;
-        }
-      else
-        builtins.fetchurl {
-          url = value.src.url;
+      src = if value.src.type == "github" then
+        fetchTarball {
+          url = "https://github.com/${value.src.owner}/${value.src.repo}/archive/${value.src.rev}.tar.gz";
           sha256 = value.src.sha256;
-        };
+        }
+            else if value.src.type == "git" then
+              fetchGit {
+                url = value.src.url;
+                rev = value.src.rev;
+                shallow = true;
+              }
+            else
+              builtins.fetchurl {
+                url = value.src.url;
+                sha256 = value.src.sha256;
+              };
     in
     value // { inherit src; }
   ) sourcesJson;
