e31226832792d19e1186c37eb9c63f3bc68c86ef
Sunny <sunnybhowmick0310@gmail.com>
2026-02-05 18:27:14 +0530

    installed budgie and regreet

--- system/packages/desktop.nix
+++ system/packages/desktop.nix
@@ -1,6 +1,14 @@
 { pkgs, sources, ... }:
 
 {
+  programs.regreet = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+  };
+  
   programs.niri.enable = true;
 
   programs.dms-shell = {
@@ -8,4 +16,8 @@
     systemd.enable = false;
     quickshell.package = (pkgs.callPackage "${sources.quickshell.src}/default.nix" {});
   };
+
+  services.desktopManager.budgie.enable = true;
+
+  environment.budgie.excludePackages = [ pkgs.vlc ];
 }
