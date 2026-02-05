9cf1d6932433f5d31d880d2f2be25c4a4497a8ce
Sunny <sunnybhowmick0310@gmail.com>
2026-02-05 15:07:47 +0530

    removed extra "noise"

--- users/sunny/default.nix
+++ users/sunny/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -18,6 +18,8 @@
     adw-gtk3
     volantes-cursors
     candy-icons
+    libsForQt5.qt5ct
+    kdePackages.qt6ct
     (writeShellScriptBin "apply-gtk-settings" ''
       ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3'"
       ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/icon-theme "'candy-icons'"
