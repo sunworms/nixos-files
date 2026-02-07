f32865809c3bb97592d350a44b1a1c99b9dd7a48
Sunny <sunnybhowmick0310@gmail.com>
2026-02-07 22:18:10 +0530

    made sources its own file with improved logic for fetching tarballs

--- default.nix
+++ default.nix
@@ -1,29 +1,7 @@
 let
   sourcesJson = builtins.fromJSON (builtins.readFile ./various/_sources/generated.json);
 
-  sources = builtins.mapAttrs (
-    name: value:
-    let
-      src =
-        if value.src.type == "github" then
-          fetchTarball {
-            url = "https://github.com/${value.src.owner}/${value.src.repo}/archive/${value.src.rev}.tar.gz";
-            sha256 = value.src.sha256;
-          }
-        else if value.src.type == "git" then
-          fetchGit {
-            url = value.src.url;
-            rev = value.src.rev;
-            shallow = true;
-          }
-        else
-          builtins.fetchurl {
-            url = value.src.url;
-            sha256 = value.src.sha256;
-          };
-    in
-    value // { inherit src; }
-  ) sourcesJson;
+  sources = import ./sources.nix sourcesJson;
 
   nixosSystem = import "${sources.nixpkgs.src}/nixos/lib/eval-config.nix";
 
