86daa0278f13636f21debe5ab09151bd0a6f18e1
Sunny <sunnybhowmick0310@gmail.com>
2026-02-22 16:12:53 +0530

    set env variables via fish instead of niri

--- users/sunny/desktop/config.fish
+++ users/sunny/desktop/config.fish
@@ -1,24 +1,38 @@
+if status is-login
+    set -gx CLUTTER_BACKEND "wayland,x11,*"
+    set -gx GDK_BACKEND "wayland,x11,*"
+    set -gx NIXOS_OZONE_WL 1
+    set -gx QT_AUTO_SCREEN_SCALE_FACTOR 1
+    set -gx QT_QPA_PLATFORM "wayland,xcb"
+    set -gx QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+    set -gx SDL_VIDEODRIVER "wayland,x11,*"
+    set -gx _JAVA_AWT_WM_NONREPARENTING 1
+    set -gx EDITOR hx
+    set -gx SUDO_EDITOR hx
+    set -gx VISUAL hx
+    set -gx GTK_THEME adw-gtk3
+    set -gx QT_QPA_PLATFORMTHEME gtk3
+end
 if status is-interactive
-    abbr --add -- emc 'emacsclient -nc'
-    abbr --add -- emt 'emacsclient -t'
     abbr --add -- grep 'grep --color=auto'
     abbr --add -- l 'ls -alh'
     abbr --add -- ll 'ls -l'
     abbr --add -- ls 'ls --color=tty'
     abbr --add -- osc 'nh clean all && sudo nix store optimise'
-    abbr --add -- oss 'nh os switch --ask -f ./default.nix'
-    abbr --add -- ost 'nh os test --ask -f ./default.nix'
+    abbr --add -- oss 'nh os switch --ask . -H'
+    abbr --add -- ost 'nh os test --ask . -H'
+    abbr --add -- lg lazygit
 
     set -U fish_greeting
     set -g fish_key_bindings fish_vi_key_bindings
     set -gx SOPS_AGE_KEY_FILE /persist/var/lib/sops-nix/key.txt
 
     function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	command yazi $argv --cwd-file="$tmp"
-	if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-	    builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
     end
 end
