2e60d6e7ca0e405350e2e9e521ffd79e5448fa77
Sunny <sunnybhowmick0310@gmail.com>
2026-03-07 16:30:54 +0530

    remove niri window switcher

--- users/sunny/desktop/scripts/default.nix
+++ users/sunny/desktop/scripts/default.nix
@@ -5,6 +5,5 @@
     (writeShellScriptBin "volume-notif" (builtins.readFile ./volume-notif.sh))
     (writeShellScriptBin "bright-notif" (builtins.readFile ./bright-notif.sh))
     (writeShellScriptBin "fuzzel-logout-menu" (builtins.readFile ./fuzzel_logout.sh))
-    (writeShellScriptBin "fuzzel-niri-window" (builtins.readFile ./niri_fuzzel_switcher.sh))
   ];
 }
