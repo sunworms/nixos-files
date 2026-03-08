947340deb27e3d44ff3cb31a6dec192a3578188a
Sunny <sunnybhowmick0310@gmail.com>
2026-03-08 18:40:31 +0530

    added env variables to config.fish

--- users/sunny/desktop/config.fish
+++ users/sunny/desktop/config.fish
@@ -1,3 +1,18 @@
+if status is-login
+    set -gx CLUTTER_BACKEND wayland
+    set -gx GDK_BACKEND wayland
+    set -gx NIXOS_OZONE_WL 1
+    set -gx QT_AUTO_SCREEN_SCALE_FACTOR 1
+    set -gx QT_QPA_PLATFORM wayland
+    set -gx QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+    set -gx SDL_VIDEODRIVER wayland
+    set -gx _JAVA_AWT_WM_NONREPARENTING 1
+    set -gx EDITOR nvim
+    set -gx SUDO_EDITOR nvim
+    set -gx VISUAL nvim
+    set -gx GTK_THEME adw-gtk3
+    set -gx QT_QPA_PLATFORMTHEME gtk3
+end
 if status is-interactive
     abbr --add -- grep 'grep --color=auto'
     abbr --add -- l 'ls -alh'
