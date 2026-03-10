bad08c4b860df99db6cf31c5cac72524e5991056
Sunny <sunnybhowmick0310@gmail.com>
2026-03-10 13:22:58 +0530

    remove manual foot and yazi template

--- users/sunny/desktop/matugen/default.nix
+++ users/sunny/desktop/matugen/default.nix
@@ -3,10 +3,8 @@
 {
   xdg.config.files = {
     "matugen/config.toml".source = ./matugen.toml;
-    "matugen/foot.ini".source = ./foot.ini;
     "matugen/template.lua".source = ./template.lua;
     "matugen/vesktop.css".source = ./vesktop.css;
-    "matugen/yazi-theme.toml".source = ./yazi-theme.toml;
     "matugen/zathura-colors".source = ./zathura-colors;
   };
 }
