609772eb14960b31f734f758e684b9fd7820585b
Sunny <sunnybhowmick0310@gmail.com>
2026-03-10 12:19:48 +0530

    switched to tor to circumvent

--- system/core/network.nix
+++ system/core/network.nix
@@ -1,22 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   networking = {
     networkmanager.enable = true;
-    nameservers = [ "127.0.0.1" ];
   };
 
-  services.dnscrypt-proxy = {
+  services.tor = {
     enable = true;
+    enableGeoIP = false;
+    client.enable = true;
+    torsocks.enable = true;
     settings = {
-      ipv6_servers = true;
-      require_dnssec = true;
-      doh_servers = true;
-      server_names = [
-        "cloudflare"
-        "google"
-        "quad9-doh-ip4-port443-nofilter-ecs-pri"
+      SocksPort = 9063;
+      UseBridges = true;
+      ClientTransportPlugin = "obfs4 exec ${pkgs.obfs4}/bin/lyrebird";
+      Bridge = [
+        "obfs4 51.75.207.79:443 EB25E719A7A1C1C1D5B8EB637B3831B2F1A0BE1B cert=PXrZbce37lFWcsqnlZqvaHaJGgAI1OXrwhPeQE2KAIfh7ZaFptgITMRBiis1HPhSnwUyCw iat-mode=0"
+        "obfs4 82.66.98.48:9003 33416F5D1A8D766154B3D8D9E838323D4876F38C cert=XKdqBbr0t2+v7a3KPO0++oz9TW2Mm4bqlxuSDbbGerbeNry2+1ANWqo4Ss63iANcRGjzCQ iat-mode=0"
       ];
     };
+  };
+
+  services.snowflake-proxy = {
+    enable = true;
+    capacity = 10;
   };
 }
