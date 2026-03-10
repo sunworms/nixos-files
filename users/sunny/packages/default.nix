2b01a482f6661219c094dad39595aa530abaff49
Sunny <sunnybhowmick0310@gmail.com>
2026-03-10 20:12:25 +0530

    create a wine64 symlink

--- users/sunny/packages/default.nix
+++ users/sunny/packages/default.nix
@@ -2,6 +2,10 @@
 
 {
   packages = with pkgs; [
+    (runCommand "wine-symlink" { } ''
+      mkdir -p $out/bin
+      ln -sf ${wineWow64Packages.stagingFull}/bin/wine $out/bin/wine64
+    '')
     wineWow64Packages.stagingFull
     winetricks
     (callPackage ./eden.nix { })
