d3e311e31836694a228214e33001963caf50893b
Sunny <sunnybhowmick0310@gmail.com>
2026-03-18 16:52:11 +0530

    removed nvim template

--- users/sunny/desktop/matugen/default.nix
+++ users/sunny/desktop/matugen/default.nix
@@ -3,7 +3,6 @@
 {
   xdg.config.files = {
     "matugen/config.toml".source = ./matugen.toml;
-    "matugen/template.lua".source = ./template.lua;
     "matugen/vesktop.css".source = ./vesktop.css;
     "matugen/zathura-colors".source = ./zathura-colors;
   };
