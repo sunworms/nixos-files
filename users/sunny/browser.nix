91a0064753b63b04191934be3a1bddc63ec20e3a
Sunny <sunnybhowmick0310@gmail.com>
2026-03-19 21:00:28 +0530

    made separate entries for proxy and unproxy helium

--- users/sunny/browser.nix
+++ users/sunny/browser.nix
@@ -1,17 +1,28 @@
 { pkgs, ... }:
 
+let
+  helium-base = pkgs.callPackage ./helium.nix { };
+
+  helium-proxied = pkgs.symlinkJoin {
+    name = "helium-proxied";
+    paths = [ helium-base ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/helium \
+        --add-flags "--no-first-run" \
+        --add-flags "--proxy-server='socks5://localhost:40000'"
+
+      mkdir -p $out/share/applications
+      substitute ${helium-base}/share/applications/helium.desktop \
+        $out/share/applications/helium-proxy.desktop \
+        --replace-warn "Name=Helium" "Name=Helium (Proxy)" \
+        --replace-warn "Exec=helium" "Exec=$out/bin/helium"
+    '';
+  };
+in
 {
   packages = [
-    (pkgs.symlinkJoin {
-      name = "helium-wrapped";
-      buildInputs = [ pkgs.makeWrapper ];
-      paths = [ (pkgs.callPackage ./helium.nix { }) ];
-      postBuild = ''
-        wrapProgram $out/bin/helium \
-          --append-flags "--no-first-run" \
-          --append-flags "--proxy-server='socks5://localhost:40000'"
-      '';
-      meta.mainProgram = "helium";
-    })
+    helium-base
+    helium-proxied
   ];
 }
