c18cb78736e5e16e4b59ed6cab7c1e7880ce6b28
Sunny <sunnybhowmick0310@gmail.com>
2026-03-24 23:31:23 +0530

    formatted

--- hosts/motobook/hardware-configuration.nix
+++ hosts/motobook/hardware-configuration.nix
@@ -15,7 +15,11 @@
   ];
 
   boot.initrd.availableKernelModules = [
-    "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"
+    "xhci_pci"
+    "nvme"
+    "usb_storage"
+    "sd_mod"
+    "rtsx_pci_sdmmc"
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
