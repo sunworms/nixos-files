c5f5658df36ed020f54876602b7c72cb9b7d88ea
Sunny <sunnybhowmick0310@gmail.com>
2026-03-26 13:40:55 +0530

    finally fixed cloudflare warp

--- system/core/network.nix
+++ system/core/network.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   networking.networkmanager.enable = true;
-  networking.nameservers = [ "127.0.0.1" ];
+  networking.firewall.checkReversePath = "loose";
 
   services.cloudflare-warp.enable = true;
 
@@ -19,50 +19,4 @@
       ];
     };
   };
-
-  services.redsocks = {
-    enable = true;
-    redsocks = [
-      {
-        port = 12345;
-        ip = "127.0.0.1";
-        proxy = "127.0.0.1:40000";
-        type = "socks5";
-      }
-    ];
-  };
-
-  systemd.services.warp-iptables = {
-    description = "iptables rules for WARP redsocks";
-    after = [ "network.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDIRECT --to-port 12345
-        ${pkgs.iptables}/bin/iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDIRECT --to-port 12345
-      '';
-      ExecStop = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D OUTPUT -p tcp --dport 80 -j REDIRECT --to-port 12345 || true
-        ${pkgs.iptables}/bin/iptables -t nat -D OUTPUT -p tcp --dport 443 -j REDIRECT --to-port 12345 || true
-      '';
-    };
-  };
-
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      if (action.id == "org.freedesktop.systemd1.manage-units" &&
-          action.lookup("unit") == "redsocks.service" &&
-          subject.isInGroup("wheel")) {
-        return polkit.Result.YES;
-      }
-    });
-    polkit.addRule(function(action, subject) {
-      if (action.id == "org.freedesktop.systemd1.manage-units" &&
-          action.lookup("unit") == "warp-iptables.service" &&
-          subject.isInGroup("wheel")) {
-        return polkit.Result.YES;
-      }
-    });
-  '';
 }
--- users/sunny/desktop/services.nix
+++ users/sunny/desktop/services.nix
@@ -1,29 +1,6 @@
 { pkgs, ... }:
 
 {
-  systemd.services.warp-redsocks = {
-    description = "WARP + Redsocks transparent proxy";
-
-    wantedBy = [ "graphical-session.target" ];
-
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = pkgs.writeShellScript "warp-start" ''
-        ${pkgs.cloudflare-warp}/bin/warp-cli connect
-        ${pkgs.coreutils}/bin/sleep 2
-        ${pkgs.systemd}/bin/systemctl start redsocks.service
-        ${pkgs.systemd}/bin/systemctl start warp-iptables.service
-      '';
-
-      ExecStop = pkgs.writeShellScript "warp-stop" ''
-        ${pkgs.systemd}/bin/systemctl stop warp-iptables.service
-        ${pkgs.systemd}/bin/systemctl stop redsocks.service
-        ${pkgs.cloudflare-warp}/bin/warp-cli disconnect
-      '';
-    };
-  };
-
   systemd.services.rclone-gdrive-mount = {
     description = "rclone: Remote FUSE mount for Google Drive";
     after = [ "default.target" ];
