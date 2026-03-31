3b4c3b15519c50259b6cc792e3c8d28595d13a74
Sunny <sunnybhowmick0310@gmail.com>
2026-03-31 22:20:11 +0530

    fixed blink.cmp settings

--- users/sunny/nvim/plugins/blink.lua
+++ users/sunny/nvim/plugins/blink.lua
@@ -3,23 +3,21 @@ return {
   event = "BufReadPre",
   after = function()
       require("blink.cmp").setup({
-          options = {
-            keymap = { preset = "super-tab" },
+          keymap = { preset = "super-tab" },
 
-            appearance = {
+          appearance = {
               nerd_font_variant = "mono",
-            },
-
-            completion = {
-              documentation = { auto_show = false },
-            },
-
-            sources = {
-              default = { "lsp", "path", "snippets", "buffer" },
-            },
-
-            fuzzy = { implementation = "prefer_rust_with_warning" },
           },
+
+          completion = {
+              documentation = { auto_show = false },
+          },
+
+          sources = {
+              default = { "lsp", "path", "snippets", "buffer" },
+          },
+
+          fuzzy = { implementation = "prefer_rust_with_warning" },
       })
   end
 }
