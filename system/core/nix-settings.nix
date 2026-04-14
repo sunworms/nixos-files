d463a00e5fa9a81da59cef3be25f9eefbd6a3ae1
Sunny <sunnybhowmick0310@gmail.com>
2026-04-14 18:09:52 +0530

    made wheel trusted users, added sops-nix cache

--- system/core/nix-settings.nix
+++ system/core/nix-settings.nix
@@ -10,13 +10,18 @@
         "flakes"
         "nix-command"
       ];
+      trusted-users = [
+        "@wheel"
+      ];
       substituters = [
         "https://noctalia.cachix.org"
         "https://helix.cachix.org"
+        "https://cache.thalheim.io"
       ];
       trusted-public-keys = [
         "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
       ];
     };
   };
