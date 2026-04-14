0797caac208bb4a79d371f5a42983500b086ab82
Sunny <sunnybhowmick0310@gmail.com>
2026-04-14 23:36:20 +0530

    removed all caches they were making things too slow

--- system/core/nix-settings.nix
+++ system/core/nix-settings.nix
@@ -13,16 +13,8 @@
       trusted-users = [
         "@wheel"
       ];
-      substituters = [
-        "https://noctalia.cachix.org"
-        "https://helix.cachix.org"
-        "https://cache.thalheim.io"
-      ];
-      trusted-public-keys = [
-        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-        "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
-      ];
+      substituters = [ ];
+      trusted-public-keys = [ ];
     };
   };
 }
