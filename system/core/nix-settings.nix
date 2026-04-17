8884a12abe873476d53d0899dffe27d76bf36635
Sunny <sunnybhowmick0310@gmail.com>
2026-04-17 22:10:40 +0530

    removed caches

--- system/core/nix-settings.nix
+++ system/core/nix-settings.nix
@@ -13,14 +13,8 @@
       trusted-users = [
         "@wheel"
       ];
-      substituters = [
-        "https://helix.cachix.org"
-        "https://noctalia.cachix.org"
-      ];
-      trusted-public-keys = [
-        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      ];
+      substituters = [ ];
+      trusted-public-keys = [ ];
     };
   };
 }
