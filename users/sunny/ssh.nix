541f840588a5d703eb94bda1f570f11c41eac615
Sunny <sunnybhowmick0310@gmail.com>
2026-04-19 18:17:36 +0530

    did some ssh changes (for AUR)

--- users/sunny/ssh.nix
+++ users/sunny/ssh.nix
@@ -2,19 +2,35 @@
 
 {
   files = {
-    ".ssh/config".text = ''
-      Host aur.archlinux.org
-          IdentityFile ~/.ssh/aur
-          User aur
-      Host git.gay
-          IdentityFile ~/.ssh/gitgay
-          User sunworms
-      Host github.com
-          IdentityFile ~/.ssh/github
-          User sunworms
-    '';
-    ".ssh/aur".source = osConfig.sops.secrets."sunny/aur_key".path;
-    ".ssh/gitgay".source = osConfig.sops.secrets."sunny/gitgay_key".path;
-    ".ssh/github".source = osConfig.sops.secrets."sunny/github_key".path;
+    ".ssh/config" = {
+      text = ''
+        Host aur.archlinux.org
+            IdentityFile ~/.ssh/aur
+            User aur
+        Host git.gay
+            IdentityFile ~/.ssh/gitgay
+            User sunworms
+        Host github.com
+            IdentityFile ~/.ssh/github
+            User sunworms
+      '';
+      type = "copy";
+      permissions = "600";
+    };
+    ".ssh/aur" = {
+      source = osConfig.sops.secrets."sunny/aur_key".path;
+      type = "copy";
+      permissions = "600";
+    };
+    ".ssh/gitgay" = {
+      source = osConfig.sops.secrets."sunny/gitgay_key".path;
+      type = "copy";
+      permissions = "600";
+    };
+    ".ssh/github" = {
+      source = osConfig.sops.secrets."sunny/github_key".path;
+      type = "copy";
+      permissions = "600";
+    };
   };
 }
