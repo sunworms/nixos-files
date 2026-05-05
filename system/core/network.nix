311efde3f385765e2e7c76950a0d1d5ae1890f92
Sunny <sunnybhowmick0310@gmail.com>
2026-05-06 00:12:15 +0530

    remove dnscrypt-proxy

--- system/core/network.nix
+++ system/core/network.nix
@@ -5,18 +5,4 @@
   networking.firewall.checkReversePath = "loose";
 
   services.cloudflare-warp.enable = true;
-
-  services.dnscrypt-proxy = {
-    enable = true;
-    settings = {
-      ipv6_servers = true;
-      require_dnssec = true;
-      doh_servers = true;
-      server_names = [
-        "cloudflare"
-        "google"
-        "quad9-doh-ip4-port443-nofilter-ecs-pri"
-      ];
-    };
-  };
 }
