cc78f312bbc631746695a648cd23dae861cb8925
Sunny <sunnybhowmick0310@gmail.com>
2026-07-02 19:35:13 +0530

    added nix logo for launcher

--- users/sunny/desktop/noctalia/default.nix
+++ users/sunny/desktop/noctalia/default.nix
@@ -9,24 +9,30 @@
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
 
     # needed for noctalia
-      gpu-screen-recorder
-      # grim
-      # slurp
-      # wl-clipboard
-      # (tesseract.override {
-        # enableLanguages = [ "eng" ];
-      # })
-      # imagemagick
-      # zbar
-      # curl
-      # translate-shell
-      # wf-recorder
-      # ffmpeg
-      # gifski
-      # wl-mirror
+    gpu-screen-recorder
+    # grim
+    # slurp
+    # wl-clipboard
+    # (tesseract.override {
+    # enableLanguages = [ "eng" ];
+    # })
+    # imagemagick
+    # zbar
+    # curl
+    # translate-shell
+    # wf-recorder
+    # ffmpeg
+    # gifski
+    # wl-mirror
   ];
 
   xdg.config.files = {
-    "noctalia/config.toml".source = ./config.toml;
+    "noctalia/config.toml".source = (
+      pkgs.replaceVars ./config.toml {
+        NIXOS_IMAGE = builtins.toJSON "${
+          inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+        }/share/noctalia/assets/images/distros/nixos.svg";
+      }
+    );
   };
 }
