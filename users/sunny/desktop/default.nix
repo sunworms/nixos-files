6e354788beb05f0f4435cd8e4135ac6cdeb69fd8
Sunny <sunnybhowmick0310@gmail.com>
2026-07-05 18:52:49 +0530

    switched to foot because i can

--- users/sunny/desktop/default.nix
+++ users/sunny/desktop/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    ./alacritty
+    ./foot
     ./fish
     ./niri
     ./noctalia
---
+++ users/sunny/desktop/foot/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  packages = with pkgs; [
+    foot
+    (runCommand "xdg-terminal-exec" { } ''
+      mkdir -p $out/bin
+      ln -sf ${foot}/bin/foot $out/bin/xdg-terminal-exec
+    '')
+  ];
+
+  xdg.config.files."foot/foot.ini".source = ./foot.ini;
+}
