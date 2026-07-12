38d1d5208a5978ce82bb4348848f16365839b426
Sunny <sunnybhowmick0310@gmail.com>
2026-07-12 19:58:06 +0530

    update pcsx2, switch from tuigreet to regreet

--- system/packages/desktop.nix
+++ system/packages/desktop.nix
@@ -1,17 +1,43 @@
 { pkgs, ... }:
 
+let
+  niri-regreet = pkgs.writeText "niri-regreet.kdl" ''
+    		input {
+    				touchpad {
+    						tap
+    						natural-scroll
+    				}
+    		}
+
+    		hotkey-overlay {
+    				skip-at-startup
+    		}
+
+    		spawn-sh-at-startup "${pkgs.regreet}/bin/regreet; ${pkgs.niri}/bin/niri msg action quit --skip-confirmation"
+    	'';
+in
 {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${
-          pkgs.callPackage ./tuigreet.nix { }
-        }/bin/tuigreet --time --remember --remember-session";
+        command = "${pkgs.dbus}/bin/dbus-run-session ${pkgs.niri}/bin/niri -c ${niri-regreet}";
       };
     };
-    useTextGreeter = true;
   };
+
+  environment.etc."greetd/regreet.toml".text = ''
+    		[GTK]
+    		application_prefer_dark_theme = true
+    		theme_name = "Adwaita"
+    		icon_theme_name = "Adwaita"
+    		cursor_theme_name = "Adwaita"
+    	'';
+
+  environment.systemPackages = [
+    pkgs.gnome-themes-extra
+    pkgs.adwaita-icon-theme
+  ];
 
   programs.niri = {
     enable = true;
