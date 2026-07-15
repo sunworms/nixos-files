af441ee39f2608e73acf1bf073a5da07c6d205e3
Sunny <sunnybhowmick0310@gmail.com>
2026-07-16 00:01:44 +0530

    change some tlp settings

--- system/core/battery.nix
+++ system/core/battery.nix
@@ -3,27 +3,60 @@
 {
   services = {
     upower.enable = true;
+    power-profiles-daemon.enable = false;
     tlp = {
       enable = true;
       settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
 
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 0;
 
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 60;
 
-        DISK_DEVICES = "nvme0n1";
+        PLATFORM_PROFILE_ON_AC = "balanced";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
+
+        SCHED_POWERSAVE_ON_AC = 0;
+        SCHED_POWERSAVE_ON_BAT = 1;
+
+        INTEL_GPU_MIN_FREQ_ON_BAT = 100;
+        INTEL_GPU_MAX_FREQ_ON_BAT = 800;
+        INTEL_GPU_BOOST_FREQ_ON_BAT = 800;
 
         PCIE_ASPM_ON_AC = "default";
         PCIE_ASPM_ON_BAT = "powersupersave";
 
-        RUNTIME_PM_ON_AC = "auto";
+        USB_AUTOSUSPEND = 1;
+        USB_EXCLUDE_BTUSB = 0;
+
+        RUNTIME_PM_ON_AC = "on";
         RUNTIME_PM_ON_BAT = "auto";
 
-        USB_AUTOSUSPEND = 1;
+        WIFI_PWR_ON_AC = "off";
+        WIFI_PWR_ON_BAT = "on";
+
+        SOUND_POWER_SAVE_ON_AC = 0;
+        SOUND_POWER_SAVE_ON_BAT = 1;
+        SOUND_POWER_SAVE_CONTROLLER = "Y";
+
+        SATA_LINKPWR_ON_AC = "med_power_with_dipm";
+        SATA_LINKPWR_ON_BAT = "min_power";
+        AHCI_RUNTIME_PM_ON_BAT = "auto";
+
+        WOL_DISABLE = "Y";
+
+        DEVICES_TO_DISABLE_ON_STARTUP_BAT = "bluetooth";
       };
     };
   };
