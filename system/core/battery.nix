536abb4c9f0c946282b2e4203eb1172a8b8df371
Sunny <sunnybhowmick0310@gmail.com>
2026-07-16 00:43:10 +0530

    more battery tweaks

--- system/core/battery.nix
+++ system/core/battery.nix
@@ -7,57 +7,38 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        STOP_CHARGE_THRESH_BAT0 = 80;
 
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
 
-        CPU_HWP_DYN_BOOST_ON_AC = 1;
-        CPU_HWP_DYN_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 60;
-
-        PLATFORM_PROFILE_ON_AC = "balanced";
-        PLATFORM_PROFILE_ON_BAT = "low-power";
-
-        SCHED_POWERSAVE_ON_AC = 0;
-        SCHED_POWERSAVE_ON_BAT = 1;
-
+        INTEL_GPU_MIN_FREQ_ON_AC = 400;
         INTEL_GPU_MIN_FREQ_ON_BAT = 100;
+        INTEL_GPU_MAX_FREQ_ON_AC = 1600;
         INTEL_GPU_MAX_FREQ_ON_BAT = 800;
-        INTEL_GPU_BOOST_FREQ_ON_BAT = 800;
 
         PCIE_ASPM_ON_AC = "default";
-        PCIE_ASPM_ON_BAT = "powersupersave";
+        PCIE_ASPM_ON_BAT = "powersave";
 
-        USB_AUTOSUSPEND = 1;
-        USB_EXCLUDE_BTUSB = 0;
-
-        RUNTIME_PM_ON_AC = "on";
-        RUNTIME_PM_ON_BAT = "auto";
-
-        WIFI_PWR_ON_AC = "off";
-        WIFI_PWR_ON_BAT = "on";
+        NVME_APST_WD_ON_AC = 0;
+        NVME_APST_WD_ON_BAT = 120000;
 
         SOUND_POWER_SAVE_ON_AC = 0;
         SOUND_POWER_SAVE_ON_BAT = 1;
         SOUND_POWER_SAVE_CONTROLLER = "Y";
 
-        SATA_LINKPWR_ON_AC = "med_power_with_dipm";
-        SATA_LINKPWR_ON_BAT = "min_power";
-        AHCI_RUNTIME_PM_ON_BAT = "auto";
+        USB_AUTOSUSPEND = 1;
 
-        WOL_DISABLE = "Y";
-
-        DEVICES_TO_DISABLE_ON_STARTUP_BAT = "bluetooth";
+        WIFI_PWR_ON_AC = "off";
+        WIFI_PWR_ON_BAT = "on";
       };
     };
+    thermald.enable = true;
   };
 }
