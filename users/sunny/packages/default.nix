008f9520abed3283eec8996d29e08e1e5c8dff59
Sunny <sunnybhowmick0310@gmail.com>
2026-07-16 20:46:46 +0530

    made services different file, might add arrpc service in future idk

--- users/sunny/packages/default.nix
+++ users/sunny/packages/default.nix
@@ -1,6 +1,10 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
+  imports = [
+    ./services.nix
+  ];
+
   packages = with pkgs; [
     (runCommand "wine-symlink" { } ''
       mkdir -p $out/bin
@@ -31,35 +35,6 @@
     qbittorrent
     zathura
     rclone
+    #arrpc
   ];
-
-  systemd.services.rclone-gdrive = {
-    description = "Mount Google Drive via rclone";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    environment = {
-      PATH = lib.mkForce "/run/wrappers/bin:/run/current-system/sw/bin:/etc/profiles/per-user/sunny/bin";
-    };
-
-    serviceConfig = {
-      Type = "notify";
-
-      ExecStartPre = ''
-        ${pkgs.coreutils}/bin/mkdir -p %h/Documents/gdrive
-      '';
-
-      ExecStart = ''
-        ${pkgs.rclone}/bin/rclone mount gdrive: %h/Documents/gdrive --vfs-cache-mode writes
-      '';
-
-      ExecStop = ''
-        ${pkgs.fuse3}/bin/fusermount3 -u %h/Documents/gdrive
-      '';
-
-      Restart = "on-failure";
-      RestartSec = 5;
-    };
-
-    wantedBy = [ "default.target" ];
-  };
 }
