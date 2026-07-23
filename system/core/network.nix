2f1e7f50329bd7526ba1ddf1c8fe2d0737202dde
Sunny <sunnybhowmick0310@gmail.com>
2026-07-24 00:09:37 +0530

    committing wgcf with ipv6 for future purposes

--- system/core/network.nix
+++ system/core/network.nix
@@ -1,6 +1,31 @@
-{...}: {
+{config, ...}: {
   networking.networkmanager.enable = true;
   networking.firewall.checkReversePath = "loose";
 
-  services.cloudflare-warp.enable = true;
+  networking.wg-quick.interfaces.warp = {
+    privateKeyFile = config.sops.secrets."root/warp-private-key".path;
+
+    address = [
+      "172.16.0.2/32"
+      "2606:4700:110:89fb:adfd:9223:9216:505b/128"
+    ];
+
+    dns = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
+
+    peers = [
+      {
+        publicKey = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
+        endpoint = "engage.cloudflareclient.com:2408";
+        allowedIPs = [
+          "0.0.0.0/0"
+          "::/0"
+        ];
+      }
+    ];
+  };
 }
