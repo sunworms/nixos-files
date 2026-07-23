c5dbf4bb5160ff947940bd183417ecfef5d4c70a
Sunny <sunnybhowmick0310@gmail.com>
2026-07-23 20:09:07 +0530

    added niri-float-sticky

--- users/sunny/desktop/niri/default.nix
+++ users/sunny/desktop/niri/default.nix
@@ -1,5 +1,13 @@
-{...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   xdg.config.files = {
     "niri/config.kdl".source = ./config.kdl;
   };
+
+  packages = [
+    (pkgs.callPackage "${inputs.niri-float-sticky}/package.nix" {})
+  ];
 }
