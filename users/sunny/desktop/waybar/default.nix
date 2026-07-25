ef3358b6e7b986224d555255a0b90c5f11eda8c6
Sunny <sunnybhowmick0310@gmail.com>
2026-07-25 17:22:54 +0530

    two vertical waybars

--- users/sunny/desktop/waybar/default.nix
+++ users/sunny/desktop/waybar/default.nix
@@ -2,10 +2,14 @@
   packages = with pkgs; [
     waybar
     networkmanagerapplet
+    wirelesstools
+    iw
+    (writeShellScriptBin "network-script" (builtins.readFile ./network-script))
   ];
 
   xdg.config.files = {
-    "waybar/config.jsonc".source = ./config.jsonc;
+    "waybar/config-1.jsonc".source = ./config-1.jsonc;
+    "waybar/config-2.jsonc".source = ./config-2.jsonc;
     "waybar/style.css".source = ./style.css;
   };
 }
