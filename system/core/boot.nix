32f433ec979c5cf768c6d85dae190dbf41aecb0b
Sunny <sunnybhowmick0310@gmail.com>
2026-08-04 21:06:06 +0530

    trying out xanmod kernel for stability

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -7,7 +7,7 @@
       };
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
     zswap = {
       enable = true;
       maxPoolPercent = 40;
