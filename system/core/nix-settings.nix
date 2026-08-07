1b861a3334db6a6eddb0b5614f1357d6ea3989f5
Sunny <sunnybhowmick0310@gmail.com>
2026-08-07 20:15:33 +0530

    removed the jobs thingie

--- system/core/nix-settings.nix
+++ system/core/nix-settings.nix
@@ -3,8 +3,6 @@
     package = pkgs.nixVersions.latest;
     channel.enable = false;
     settings = {
-      max-jobs = 4;
-      cores = 6;
       auto-optimise-store = true;
       experimental-features = [
         "flakes"
