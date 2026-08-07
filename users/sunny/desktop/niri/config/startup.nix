e891508f14e97b2b2bd15f27689d85ce2763aa49
Sunny <sunnybhowmick0310@gmail.com>
2026-08-07 22:26:05 +0530

    fixed foot spawning before noctalia ran

--- users/sunny/desktop/niri/config/startup.nix
+++ users/sunny/desktop/niri/config/startup.nix
@@ -2,10 +2,6 @@
   spawn-sh-at-startup = [
     {_args = ["niri-float-sticky -title 'Picture-in-Picture|Picture in picture'"];}
     {_args = ["sleep 0.5 && kanata --cfg ~/.config/kanata/config.kbd"];}
-  ];
-
-  spawn-at-startup = [
-    {_args = ["noctalia" "--daemon"];}
-    {_args = ["foot" "--server"];}
+    {_args = ["noctalia --daemon && foot --server"];}
   ];
 }
