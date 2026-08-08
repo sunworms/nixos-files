d635c1cc8ff41d61af3fc90bd9627c6b02bcee3b
Sunny <sunnybhowmick0310@gmail.com>
2026-08-09 00:10:25 +0530

    changed some plugin settings

--- users/sunny/desktop/noctalia/config/plugins.nix
+++ users/sunny/desktop/noctalia/config/plugins.nix
@@ -15,5 +15,17 @@
     "noctalia/mpvpaper" = {
       video_directory = "/home/sunny/Pictures/walls/videos";
     };
+    "alexander/screen-toolkit" = {
+      panel-full_placement = "attached";
+      panel-legacy_placement = "attached";
+      panel_placement = "attached";
+      result_placement = "attached";
+    };
+    "cleboost/hotspot" = {
+      panel_open_near_click = false;
+    };
+    "elijaharch/wl-screen-mirror" = {
+      controls_open_near_click = false;
+    };
   };
 }
