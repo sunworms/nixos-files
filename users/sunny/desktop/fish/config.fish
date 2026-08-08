3a401570d067b9fb5ff54a85095f14194958847e
Sunny <sunnybhowmick0310@gmail.com>
2026-08-08 14:19:04 +0530

    cleaned up fish config a bit

--- users/sunny/desktop/fish/config.fish
+++ users/sunny/desktop/fish/config.fish
@@ -1,18 +1,12 @@
-if status is-interactive
-    abbr --add -- grep 'grep --color=auto'
-    abbr --add -- l 'ls -alh'
-    abbr --add -- ll 'ls -l'
-    abbr --add -- ls 'ls --color=tty'
-    abbr --add -- osb 'nh os boot --ask -f ./.'
-    abbr --add -- osbu 'nh os build -f ./.'
-    abbr --add -- osca 'nh clean all'
-    abbr --add -- oscd 'nh clean all --no-direnv'
-    abbr --add -- oss 'nh os switch --ask -f ./.'
-    abbr --add -- ost 'nh os test --ask -f ./.'
+abbr l 'ls -alh'
+abbr ll 'ls -l'
+abbr ls 'ls --color=tty'
+abbr osb 'nh os boot --ask -f ./.'
+abbr osbu 'nh os build -f ./.'
+abbr osca 'nh clean all'
+abbr oscd 'nh clean all --no-direnv'
+abbr oss 'nh os switch --ask -f ./.'
+abbr ost 'nh os test --ask -f ./.'
 
-    alias vi nvim
-    alias vim nvim
-
-    set -g fish_greeting
-    set -g fish_key_bindings fish_vi_key_bindings
-end
+set -g fish_greeting
+set -g fish_key_bindings fish_vi_key_bindings
