f9cb78ebf64782f585758296e6337de508129c54
Sunny <sunnybhowmick0310@gmail.com>
2026-08-10 00:09:38 +0530

    watt 1.4.0 is in nixpkgs now

--- system/core/battery.nix
+++ system/core/battery.nix
@@ -1,13 +1,6 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{...}: {
   services = {
     upower.enable = true;
-    watt = {
-      enable = true;
-      package = pkgs.callPackage "${inputs.watt}/nix/package.nix" {};
-    };
+    watt.enable = true;
   };
 }
