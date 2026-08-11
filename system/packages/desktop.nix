1f8458017db9e11e99a34c4849df382f7b4a35a2
Sunny <sunnybhowmick0310@gmail.com>
2026-08-11 17:37:46 +0530

    just went to original tuigreet

--- system/packages/desktop.nix
+++ system/packages/desktop.nix
@@ -7,16 +7,11 @@
     enable = true;
     settings = {
       default_session = {
-        command = "/usr/bin/env XCURSOR_SIZE=48 XCURSOR_THEME=volantes_cursors QT_SCALE_FACTOR=1.75 ${lib.getExe pkgs.cage} -s -d -- ${lib.getExe pkgs.qtgreet}";
+        command = "${lib.getExe pkgs.tuigreet} --time --remember --remember-session";
         user = "greeter";
       };
     };
   };
-
-  # let greeter user access ~/.face
-  systemd.tmpfiles.rules = [
-    "z /home/sunny 711 sunny users -"
-  ];
 
   environment.systemPackages = with pkgs; [
     volantes-cursors
