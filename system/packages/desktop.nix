397b1365e93bb31f68edde9b58981b50ad068560
Sunny <sunnybhowmick0310@gmail.com>
2026-08-12 20:55:38 +0530

    qtgreet?

--- system/packages/desktop.nix
+++ system/packages/desktop.nix
@@ -1,9 +1,26 @@
-{pkgs, ...}: {
-  # so UWSM does not complain
-  systemd.targets.graphical = {
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  services.greetd = {
     enable = true;
-    wantedBy = ["multi-user.target"];
+    settings = {
+      default_session = {
+        command = "/usr/bin/env XCURSOR_SIZE=48 XCURSOR_THEME=volantes_cursors QT_SCALE_FACTOR=1.75 ${lib.getExe pkgs.cage} -s -d -- ${lib.getExe pkgs.qtgreet}";
+        user = "greeter";
+      };
+    };
   };
+
+  # let greeter user access ~/.face
+  systemd.tmpfiles.rules = [
+    "z /home/sunny 711 sunny users -"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    volantes-cursors
+  ];
 
   programs.niri = {
     enable = true;
