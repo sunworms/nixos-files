98c80b00b8f75e8a2c029abc67eda3a185cf567c
Sunny <sunnybhowmick0310@gmail.com>
2026-08-16 20:47:26 +0530

    move aliases and abbrs out of interactive shell init

--- users/sunny/desktop/fish/config.fish
+++ users/sunny/desktop/fish/config.fish
@@ -1,12 +1,12 @@
 if status is-interactive
-    alias ls lsd
-    abbr osb 'nh os boot --ask -f ./.'
-    abbr osbu 'nh os build -f ./.'
-    abbr osca 'nh clean all'
-    abbr oscd 'nh clean all --no-direnv'
-    abbr oss 'nh os switch --ask -f ./.'
-    abbr ost 'nh os test --ask -f ./.'
-
     set -g fish_greeting
     set -g fish_key_bindings fish_vi_key_bindings
 end
+
+alias ls lsd
+abbr osb 'nh os boot --ask -f ./.'
+abbr osbu 'nh os build -f ./.'
+abbr osca 'nh clean all'
+abbr oscd 'nh clean all --no-direnv'
+abbr oss 'nh os switch --ask -f ./.'
+abbr ost 'nh os test --ask -f ./.'
