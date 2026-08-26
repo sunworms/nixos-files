6be05a9435fdae250e906465e1d3bce20e7e8fd1
Sunny <sunnybhowmick0310@gmail.com>
2026-08-26 17:24:03 +0530

    some cleanup

--- users/sunny/desktop/umbriel/config/other-binds.nix
+++ users/sunny/desktop/umbriel/config/other-binds.nix
@@ -33,25 +33,6 @@
     "Mod+WheelUp" = "workspace-previous";
     "Mod+WheelDown" = "workspace-next";
 
-    "Mod+A" = "spawn:noctalia msg panel-toggle launcher";
-    "Mod+Escape" = "spawn:noctalia msg panel-toggle session";
-    "Mod+V" = "spawn:noctalia msg panel-toggle clipboard";
-    "XF86Calculator" = "spawn:noctalia msg panel-toggle control-center calendar";
-    "XF86Favorites" = "spawn:noctalia msg panel-toggle launcher /emo";
-    "XF86Launch5" = "spawn:noctalia msg session lock";
-    "XF86Launch6" = "spawn:noctalia msg window-switcher";
-    "XF86PowerOff" = "spawn:noctalia msg session lock-and-suspend";
-    "XF86Tools" = "spawn:noctalia msg settings-toggle";
-    "XF86AudioLowerVolume" = "spawn:noctalia msg volume-down 5";
-    "XF86AudioMicMute" = "spawn:noctalia msg mic-mute";
-    "XF86AudioMute" = "spawn:noctalia msg volume-mute";
-    "XF86AudioNext" = "spawn:noctalia msg media next";
-    "XF86AudioPlay" = "spawn:noctalia msg media toggle";
-    "XF86AudioPrev" = "spawn:noctalia msg media previous";
-    "XF86AudioRaiseVolume" = "spawn:noctalia msg volume-up 5";
-    "XF86MonBrightnessDown" = "spawn:noctalia msg brightness-down 5";
-    "XF86MonBrightnessUp" = "spawn:noctalia msg brightness-up 5";
-
     "Mod+Alt+V" = "window-toggle-floating";
     "Mod+Comma" = "window-consume-left";
     "Mod+Period" = "window-expel-right";
--- users/sunny/desktop/umbriel/config/user-binds.nix
+++ users/sunny/desktop/umbriel/config/user-binds.nix
@@ -19,5 +19,24 @@
     "Mod+Shift+T" = "spawn:foot sh -c \"umbriel layers; fish\"";
 
     "Mod+T" = "spawn:foot";
+
+    "Mod+A" = "spawn:noctalia msg panel-toggle launcher";
+    "Mod+Escape" = "spawn:noctalia msg panel-toggle session";
+    "Mod+V" = "spawn:noctalia msg panel-toggle clipboard";
+    "XF86Calculator" = "spawn:noctalia msg panel-toggle control-center calendar";
+    "XF86Favorites" = "spawn:noctalia msg panel-toggle launcher /emo";
+    "XF86Launch5" = "spawn:noctalia msg session lock";
+    "XF86Launch6" = "spawn:noctalia msg window-switcher";
+    "XF86PowerOff" = "spawn:noctalia msg session lock-and-suspend";
+    "XF86Tools" = "spawn:noctalia msg settings-toggle";
+    "XF86AudioLowerVolume" = "spawn:noctalia msg volume-down 5";
+    "XF86AudioMicMute" = "spawn:noctalia msg mic-mute";
+    "XF86AudioMute" = "spawn:noctalia msg volume-mute";
+    "XF86AudioNext" = "spawn:noctalia msg media next";
+    "XF86AudioPlay" = "spawn:noctalia msg media toggle";
+    "XF86AudioPrev" = "spawn:noctalia msg media previous";
+    "XF86AudioRaiseVolume" = "spawn:noctalia msg volume-up 5";
+    "XF86MonBrightnessDown" = "spawn:noctalia msg brightness-down 5";
+    "XF86MonBrightnessUp" = "spawn:noctalia msg brightness-up 5";
   };
 }
