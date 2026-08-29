21a02d51c481d3de1fec6357cbd6ab9ab27522d9
Sunny <sunnybhowmick0310@gmail.com>
2026-08-29 13:56:59 +0530

    remove dwindle layouts

--- users/sunny/desktop/umbriel/config/layout.nix
+++ users/sunny/desktop/umbriel/config/layout.nix
@@ -2,19 +2,12 @@
   layout = {
     mode = "scrolling";
     gap = 5;
-    width_presets = [0.333 0.5 0.667 1.0];
+    width_presets = [0.25 0.333 0.5 0.667 0.75 1.0];
     scrolling = {
       default_width_fraction = 1.0;
     };
+    master = {
+      default_width_fraction = 0.75;
+    };
   };
-  workspace = [
-    {
-      index = 2;
-      layout.mode = "dwindle";
-    }
-    {
-      index = 4;
-      layout.mode = "dwindle";
-    }
-  ];
 }
