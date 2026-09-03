e8fd8bbf286a917b7bf651281030b35d645570c5
Sunny <sunnybhowmick0310@gmail.com>
2026-09-03 20:08:45 +0530

    idk useless change

--- system/core/network.nix
+++ system/core/network.nix
@@ -1,5 +1,6 @@
 {...}: {
   networking.networkmanager.enable = true;
+
   networking.firewall.checkReversePath = "loose";
 
   services.cloudflare-warp.enable = true;
--- users/sunny/desktop/fish/default.nix
+++ users/sunny/desktop/fish/default.nix
@@ -1,6 +1,19 @@
 {pkgs, ...}: {
   xdg.config.files = {
-    "fish/config.fish".source = ./config.fish;
+    "fish/config.fish".text =
+      #fish
+      ''
+        set -g fish_greeting
+        set -g fish_key_bindings fish_vi_key_bindings
+
+        abbr osb 'nh os boot --ask -f ./.'
+        abbr osbu 'nh os build -f ./.'
+        abbr osca 'nh clean all'
+        abbr oscd 'nh clean all --no-direnv'
+        abbr oss 'nh os switch --ask -f ./.'
+        abbr ost 'nh os test --ask -f ./.'
+        abbr lg lazygit
+      '';
     "fish/functions".source = ./functions;
   };
 
