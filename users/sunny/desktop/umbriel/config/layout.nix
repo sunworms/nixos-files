47a57ed1ce6e3cfe53945a9929235423d435de15
Sunny <sunnybhowmick0310@gmail.com>
2026-09-05 20:38:59 +0530

    added struts

--- users/sunny/desktop/umbriel/config/layout.nix
+++ users/sunny/desktop/umbriel/config/layout.nix
@@ -1,13 +1,18 @@
 {
   layout = {
     mode = "scrolling";
-    gap = 5;
     width_presets = [0.25 0.333 0.5 0.667 0.75 1.0];
     scrolling = {
       default_width_fraction = 1.0;
     };
     master = {
       default_width_fraction = 0.75;
+    };
+    struts = {
+      left = 10;
+      right = 10;
+      top = 10;
+      bottom = 10;
     };
   };
 }
