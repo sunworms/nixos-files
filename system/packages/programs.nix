02233e3a9faad084776e2a339ef7f0c1a37ef9b9
Sunny <sunnybhowmick0310@gmail.com>
2026-09-14 20:47:12 +0530

    moved gsettings env to system level

--- system/packages/programs.nix
+++ system/packages/programs.nix
@@ -57,6 +57,10 @@
     wl-clipboard
   ];
 
+  environment.sessionVariables = {
+    GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
+  };
+
   environment.pathsToLink = [
     "/share/xdg-desktop-portal"
     "/share/applications"
